2c87ed1da9cb81b350e9252a5dc3c99290724ee0
Camden Fullmer <camdenfullmer@gmail.com>
2012-10-08 21:59:57 -0400

    Can successfully receive the html page with the student's schedule.

--- KSU Balance Inquiry/app.py
+++ KSU Balance Inquiry/app.py
@@ -1,5 +1,6 @@
 import urllib
 import urllib2
+import ClientCookie
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,50 +8,31 @@ app = Flask(__name__)
 @app.route("/")
 def start():
     # Root url for the account service
-    root_url = 'https://services.jsatech.com'
+    root_url = 'https://keys.kent.edu:44220/ePROD'
     
-    # STAGE 1
-    url = root_url + '/login.php';
-    values = {'cid':40,'save':1,'loginphrase':'cfullmer','password':'Gyroman1','x':1,'y':1}
+    # Retrieve the login page.
+    url = root_url + '/twbkwbis.P_WWWLogin'
+    req = urllib2.Request(url)
+    response = ClientCookie.urlopen(req)
+    the_page = response.read()
+    
+    # Login into the service.
+    url = root_url + '/twbkwbis.P_ValLogin'
+    values = {'sid':'cfullmer','PIN':'Gyroman1'}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)  
+    response = ClientCookie.urlopen(req)
+    the_page = response.read()
+    
+    # Get the HTML for the schedule page.
+    url = root_url + '/bwskfshd.P_CrseSchdDetl'
+    values = {'term_in':'201280'}
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
+    response = ClientCookie.urlopen(req)
     the_page = response.read()
-    key_index = the_page.rfind('window.location.href=')
-    login_url = the_page[key_index + 22:key_index + 97]
-    key_cid = the_page[key_index + 32:key_index + 78]
-    key = the_page[key_index + 32:key_index + 70]
-    print login_url
-    
-    #STAGE 2
-    url = root_url + login_url;
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    print the_page
-    
-    #STAGE 3
-    url = root_url + '/login-check.php' + key;
-    print url
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    print the_page
-    
-    #STAGE 4
-    url = root_url + '/index.php' + key_cid;
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    print the_page
-    print the_page[the_page.rfind('Current Balance:'):the_page.rfind('Current Balance:') + 28]
-    
-    url = root_url + '/logout.php' + key_cid;
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    #print the_page
-    
+    print the_page    
+
     return 'Hello World'
 
 if __name__ == "__main__":
